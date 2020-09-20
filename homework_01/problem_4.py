9f5b51fa08710c82602bedc033b5b80714ecb2b1
Trung Hiếu <45393995+hakunhan@users.noreply.github.com>
2020-09-20 18:47:11 +0700

    Answer of homework week 1
    I have finished problem 4 to problem 9 with python coding

---
+++ homework_01/problem_4.py
@@ -0,0 +1,3 @@
+total_sale = float(input("Enter the total sale: "))
+predict = f"{total_sale*1.23}"
+print("Profit will be made form the total sale is: " + predict)
---
+++ homework_01/problem_5.py
@@ -0,0 +1,3 @@
+total_square_feet = float(input("Enter the total square feet of the tract of land: "))
+calculate_acres = f"{total_square_feet / 43500:.2f}"
+print("The number of arches of that tract of land is: " + calculate_acres)
