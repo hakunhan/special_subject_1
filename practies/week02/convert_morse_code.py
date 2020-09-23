c510b031a68cc1df797adca708c293bc449f6ddb
hakunhan <hakunhan@gmail.com>
2020-09-23 09:29:25 +0700

    done class A, problem 1,2

---
+++ practies/week02/convert_morse_code.py
@@ -0,0 +1,36 @@
+#converting alphabet string to morse code
+
+#dictionary of morse code
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+#converting input string to morse code
+def convert(str):
+    result = ''
+
+    for letter in str:
+        if letter == ' ':
+            result += letter
+        else:
+            result += MORSE_CODE_DICT[letter.upper()]
+
+    return result
+
+#get user input
+def main():
+    str = input("Enter the string need to convert: ")
+    print(convert(str))
+
+main()
