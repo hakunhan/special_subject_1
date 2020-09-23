59a81ab8170232cb948f54f680b3f2bc36f98d51
Trung Hiếu <45393995+hakunhan@users.noreply.github.com>
2020-09-23 18:56:31 +0700

    Done problem 4

---
+++ homework_02/problem4.py
@@ -0,0 +1,18 @@
+#capitalize first character of each sentences
+def capitalize(sentences):
+    result = ""
+    #split the sentences into list of sentence
+    list_sentence = sentences.split(" ")
+    print(list_sentence)
+
+    for i in range(len(list_sentence)):
+        #substring the sentence into first char that will be capitalize and the others then add into result
+        result += list_sentence[i][0].upper() + list_sentence[i][1:len(list_sentence[i])] + " "
+
+    return result
+
+def main():
+    sentences = input("Enter your sentences: ")
+    print(capitalize(sentences))
+
+main()
