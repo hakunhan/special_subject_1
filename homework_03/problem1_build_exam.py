ecc0b4d7b5baaf094d5015faee8126e48319e4ca
Trung Hiếu <45393995+hakunhan@users.noreply.github.com>
2020-10-04 18:54:08 +0700

    writing requirement

---
+++ homework_03/problem1_build_exam.py
@@ -0,0 +1,8 @@
+#creating application to auto grade students answer
+#base on giving correct answers
+
+#requirement: read students answer file, display a message contain:
+#1. indication if student pass the test or not (minimum 15 correct answers to pass)
+#2. total correct answer, incorrect answer
+#3. list of incorrect answer
+
