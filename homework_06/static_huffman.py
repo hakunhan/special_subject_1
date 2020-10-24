6a9190d65e88dc05033b3081dc69df2f9c34d7a8
Trung Hiếu <45393995+hakunhan@users.noreply.github.com>
2020-10-24 20:43:14 +0700

    Update static_huffman

--- homework_06/static_huffman.py
+++ homework_06/static_huffman.py
@@ -1,5 +1,5 @@
 # compressing text file using static huffman algorithm
-import utils.data_structure.binary_tree
+import utils.data_structure.example_binary_tree
 
 def get_frequency_dict(string):
     frequency_dict = {}
@@ -16,22 +16,46 @@ def get_frequency_dict(string):
 
     return dict(sorted(frequency_dict.items(), key = lambda x : x[1], reverse=False))
 
+class binary_tree():
+    def __init__(self, key):
+        self.key = key
+        self
+
 def static_huffman(my_dict):
-    binary_tree = utils.data_structure.binary_tree.AVLTree()
+    b_tree = binary_tree()
+    b_tree.insert("1")
 
     static_huffman_dict = my_dict
 
     for i in range (len(my_dict)):
         __value, __key = None, None
+        b_tree.insert(list(static_huffman_dict.keys())[0])
 
-        
+        if(len(static_huffman_dict) > 1):
+            b_tree.insert(list(static_huffman_dict.keys())[1])
+
+        if(len(static_huffman_dict) > 1):
+            __value = list(static_huffman_dict.values())[0] + list(static_huffman_dict.values())[1]
+            __key = list(static_huffman_dict.keys())[0] + list(static_huffman_dict.keys())[1]
+        else:
+            __value = list(static_huffman_dict.values())[0]
+            __key = list(static_huffman_dict.keys())[0]
 
 
-    return binary_tree
+        static_huffman_dict.pop(list(static_huffman_dict.keys())[0])
+
+        if(len(static_huffman_dict) > 1):
+            static_huffman_dict.pop(list(static_huffman_dict.keys())[0])
+
+        static_huffman_dict[__key] = __value
+        dict(sorted(static_huffman_dict.items(), key = lambda x: x[1]), reverse = False)
+        print(static_huffman_dict)
+
+    return b_tree
 
 my_dict = get_frequency_dict("asdgfasdfasfasdf")
 print(my_dict)
-print(list(my_dict.keys())[0])
 
-arr = static_huffman(my_dict).inorder_traverse()
-print(arr)
+arr = static_huffman(my_dict)
+print(arr.inorder_traverse())
+print(arr.display())
