50c3935aa8acd02647fff5b5aabced0b9b972283
Trung Hiếu <45393995+hakunhan@users.noreply.github.com>
2020-10-26 16:34:48 +0700

    Rework static huffman

--- homework_06/static_huffman.py
+++ homework_06/static_huffman.py
@@ -1,9 +1,42 @@
 # compressing text file using static huffman algorithm
 import utils.data_structure.binary_tree
 
+class Node():
+    def __init__(self, data=None, frequency=0, left=None, right=None):
+        self.data = data
+        self.frequency = frequency
+        self.left = left
+        self.right = right
+
+    def printTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.data),
+        if self.right:
+            self.right.PrintTree()
+
+class Binary_tree():
+    def __init__(self):
+        self.node = None
+
+    def insert(self, data = None, frequency = 0):
+        tree = self.node
+
+        if tree == None:
+            self.node = Node(data,frequency)
+            self.node.left = Binary_tree()
+            self.node.right = Binary_tree()
+
+        elif frequency < tree.frequency:
+            self.node.left.insert(data,frequency)
+
+        elif frequency > tree.frequency:
+            self.node.right.insert(data,frequency)
+
+
 class Static_huffman():
-    def __init__(self, string):
-        self.input_string = string
+    def __init__(self, input_string):
+        self.input_string = input_string
 
     def get_frequency_dict(self):
         frequency_dict = {}
@@ -20,58 +53,27 @@ class Static_huffman():
 
         return dict(sorted(frequency_dict.items(), key=lambda x: x[1], reverse=False))
 
-    def __generate_tree(self):
-        binary_tree = utils.data_structure.binary_tree.AVLTree()
+    def build_huffman_tree(self):
+        huffman_dict = self.get_frequency_dict()
+        tree = Binary_tree()
 
-        static_huffman_dict = self.get_frequency_dict()
+        while(len(huffman_dict) > 1):
+            tree.insert(list(huffman_dict.keys())[1], list(huffman_dict.values())[1])
+            __value = list(huffman_dict.values())[1] + list(huffman_dict.values())[1]
+            #tree.insert('\0',__value)
+            tree.insert(list(huffman_dict.keys())[0],list(huffman_dict.values())[1])
 
-        for i in range(len(static_huffman_dict) - 1):
-            __value, __key = None, None
-            binary_tree.insert(list(static_huffman_dict.values())[0])
+            huffman_dict.pop(list(huffman_dict.keys())[0])
+            huffman_dict.pop(list(huffman_dict.keys())[1])
+            huffman_dict['\0'] = __value
 
-            if (len(static_huffman_dict) > 1):
-                binary_tree.insert(list(static_huffman_dict.values())[1])
-
-            if (len(static_huffman_dict) > 1):
-                __value = list(static_huffman_dict.values())[0] + list(static_huffman_dict.values())[1]
-                __key = list(static_huffman_dict.keys())[0] + list(static_huffman_dict.keys())[1]
-                binary_tree.insert(__value)
-            else:
-                __value = list(static_huffman_dict.values())[0]
-                __key = list(static_huffman_dict.keys())[0]
-
-            static_huffman_dict.pop(list(static_huffman_dict.keys())[0])
-
-            if (len(static_huffman_dict) > 1):
-                static_huffman_dict.pop(list(static_huffman_dict.keys())[0])
-
-            static_huffman_dict[__key] = __value
-            static_huffman_dict = dict({k: v for k, v in sorted(static_huffman_dict.items(), key=lambda item: item[1])})
-
-        return binary_tree
-
-    def encode(self):
-        encode_dict = self.get_frequency_dict()
-        arr = self.__generate_tree().inorder_traverse()
-
-        encode_binary = ""
-        for i in range(len(arr) - 1, -1, -1):
-            if (i % 2 == 1):
-                encode_dict.update({list(encode_dict.keys())[i]: encode_binary+"1"})
-            else:
-                encode_dict.update({list(encode_dict.keys())[i]: encode_binary+"0"})
-        
-
-
-        return encode_dict
-
+        return tree
 
 if __name__ == "__main__":
     string = input("Enter string need to compress: ")
 
     static_huffman = Static_huffman(string)
     frequency_dict = static_huffman.get_frequency_dict()
-    encode_dict = static_huffman.encode()
+    tree = static_huffman.build_huffman_tree()
+    print(tree.node)
 
-    print(frequency_dict)
-    print(encode_dict)
